dbb190e40d38a3e45ff31f3a72d08af67ebd7a1a
Юрий Голенков <yur.golenkoff@yandex.ru>
2023-06-09 15:34:09 +0300

    change

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -92,8 +92,12 @@ class UserPostSerializer(UserCreateSerializer):
     """Сериализатор для создания пользователя."""
     class Meta:
         model = User
-        fields = tuple(User.REQUIRED_FIELDS) + (
-            User.USERNAME_FIELD,
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
             'password',
         )
 

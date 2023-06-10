5ca9cec463c9e1b24e95d86b12e14b9892b415eb
Юрий Голенков <yur.golenkoff@yandex.ru>
2023-06-10 16:18:53 +0300

    change

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,19 +2,22 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .views import (IngredientViewSet, TagViewSet,
-                    RecipeViewSet, CustomUserViewSet)
+                    RecipeViewSet, SubscriptionsView, FollowUserView)
 
 app_name = 'api'
 
 router = DefaultRouter()
 
-router.register('users', CustomUserViewSet, basename='users')
+# router.register('users', CustomUserViewSet, basename='users')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 router.register('tags', TagViewSet, basename='tags')
 router.register('recipes', RecipeViewSet, basename='recipes')
 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('users/subscriptions/', SubscriptionsView.as_view()),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('', include(router.urls)),
+    path('users/<int:id>/subscribe/', FollowUserView.as_view()),
 ]
